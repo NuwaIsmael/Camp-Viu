7ff4af7d0d5f98d2805e44e105ae4060e78c023b
Ismael <isgalvez24@gmail.com>
2021-06-05 20:54:43 +0200

    Saving

--- app/controllers/pmarket_controller.rb
+++ app/controllers/pmarket_controller.rb
@@ -7,10 +7,7 @@ class PmarketController < BaseController
     if ContentConfig.home_show_stats
       @num_distributors = cached_count('distributors', Enterprise.is_distributor.activated.visible)
       @num_producers = cached_count('producers', Enterprise.is_primary_producer.activated.visible)
-      @num_orders = cached_count('orders', Spree::Order.complete)
-      @num_users = cached_count(
-        'users', Spree::Order.complete.select('DISTINCT spree_orders.user_id')
-      )
+      @num_products = cached_count('products', "SELECT * FROM spree_products")
     end
   end
 
