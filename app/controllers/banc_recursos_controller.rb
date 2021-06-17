23fbcac6f79d555644249b1e44d8c0417502ffc1
Ismael <isgalvez24@gmail.com>
2021-06-17 12:56:34 +0200

    Netejar controladors

--- app/controllers/banc_recursos_controller.rb
+++ app/controllers/banc_recursos_controller.rb
@@ -1,29 +1,4 @@
 class BancRecursosController < BaseController
   layout 'darkswarm'
 
-  before_action :enable_embedded_shopfront
-
-  def index
-    @langvar = I18n.locale
-    if ContentConfig.home_show_stats
-      @num_distributors = cached_count('distributors', Enterprise.is_distributor.activated.visible)
-      @num_producers = cached_count('producers', Enterprise.is_primary_producer.activated.visible)
-      @num_products = 0
-    end
-  end
-
-  def sell; end
-
-  def unauthorized
-    render 'shared/unauthorized', status: :unauthorized
-  end
-
-  private
-
-  # Cache the value of the query count
-  def cached_count(statistic, query)
-    CacheService.home_stats(statistic) do
-      query.count
-    end
-  end
 end
--- app/controllers/banc_terres_controller.rb
+++ app/controllers/banc_terres_controller.rb
@@ -1,26 +1,4 @@
 class BancTerresController < BaseController
   layout 'darkswarm'
 
-  before_action :enable_embedded_shopfront
-
-  def index
-    @langvar = I18n.locale
-    if ContentConfig.home_show_stats
-    end
-  end
-
-  def sell; end
-
-  def unauthorized
-    render 'shared/unauthorized', status: :unauthorized
-  end
-
-  private
-
-  # Cache the value of the query count
-  def cached_count(statistic, query)
-    CacheService.home_stats(statistic) do
-      query.count
-    end
-  end
 end
