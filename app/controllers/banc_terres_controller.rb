a944fcf1264760122c314745dcf3d50428fef3a5
NuwaIsmael <85364118+NuwaIsmael@users.noreply.github.com>
2021-10-05 20:01:01 +0200

    Update banc_terres_controller.rb

--- app/controllers/banc_terres_controller.rb
+++ app/controllers/banc_terres_controller.rb
@@ -1,4 +1,24 @@
 class BancTerresController < BaseController
   layout 'darkswarm'
 
+  before_action :enable_embedded_shopfront
+
+  def index
+    @langvar = I18n.locale
+  end
+
+  def sell; end
+
+  def unauthorized
+    render 'shared/unauthorized', status: :unauthorized
+  end
+
+  private
+
+  # Cache the value of the query count
+  def cached_count(statistic, query)
+    CacheService.home_stats(statistic) do
+      query.count
+    end
+  end
 end
