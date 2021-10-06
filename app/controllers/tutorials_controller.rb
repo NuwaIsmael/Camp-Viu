b4ec003e7e1dc14dbbb74fa2ca1bb9bd933faa29
NuwaIsmael <85364118+NuwaIsmael@users.noreply.github.com>
2021-10-06 15:47:16 +0200

    Update tutorials_controller.rb

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,3 +1,16 @@
-class TutorialsController < BaseController
+class BancTerresController < BaseController
   layout 'darkswarm'
+
+  before_action :enable_embedded_shopfront
+
+  def index
+    @langvar = I18n.locale
+  end
+
+  def sell; end
+
+  def unauthorized
+    render 'shared/unauthorized', status: :unauthorized
+  end
+  
 end
